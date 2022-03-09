2ea3c65e98c30208b7237d406daf0d56c632e813
Davesweb <davesweb@gmail.com>
2022-03-09 22:22:03 +0100

    Install Livewire and Alpine js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,20 +3,23 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>Wordle - Spartner</title>
+        @livewireStyles
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <div class="container mx-auto p-1">
-            <div class="w-full md:w-6/12 lg:w-3/12 mx-auto">
-                <h1 class="font-medium text-3xl flex justify-between border-b border-slate-200">
+            <div class="w-full md:w-6/12 lg:w-3/12 mx-auto p-2 md:p-0">
+                <h1 class="font-medium text-3xl flex justify-between border-b border-slate-200 mb-5">
                     <span>
                         Wordle<span class="text-green-500">Game</span>
                     </span>
                     <span class="text-orange-500"><a href="https://spartner.nl/" target="_blank">Spartner</a></span>
                 </h1>
-                @yield('content')
+                @yield('content', $slot ?? '')
             </div>
         </div>
+        @livewireScripts
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
